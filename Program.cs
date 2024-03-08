87a5f430c9428b0407b8f8799892e8a5dd390d1a
Rafael_Fedora <23090555alumno.utmetropolitana@gmail.com>
2024-03-08 11:07:00 -0600

    Prueba

--- Program.cs
+++ Program.cs
@@ -7,5 +7,6 @@ class Program
         Console.WriteLine("Hola estoy probando los repositorios");
 
         Console.WriteLine("Anda bien lenta mi lentium");
+        Console.WriteLine("HELLO WORD");
     }
 }
