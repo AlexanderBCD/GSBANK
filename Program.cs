17fab80c6aa55f9be11fe9fcf0a07b6a47c0d180
Naturin02 <arturo.realeza02@gmail.com>
2024-03-08 11:26:56 -0600

    Teleton
    Se agrego un teleton para dar a entender que hay niños raros

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.WriteLine("Hola estoy probando los repositorios");
 
         Console.WriteLine("Anda bien lenta mi lentium");
-        Console.WriteLine("HELLO WORD");
         Console.Write("Hola como estas yo estoy muy bien xdxdxdxd probando si realiza los cambios");
+        Console.Write("Teleton")
     }
 }
