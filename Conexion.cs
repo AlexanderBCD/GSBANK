ee293ca7822a50983e2dbab6c3b13b007ccf20cf
Rafael_Windows <23090555alumno.utmetropolitana@gmail.com>
2024-03-13 16:54:22 -0600

    Strings conection

--- Conexion.cs
+++ Conexion.cs
@@ -7,11 +7,14 @@ public class ConexionBD
     //Conexion RafaPC [NO BORRAR]
     //private string connectionString ="Server=DESKTOP-KEI8B51;Database=bancoDeSangre;Integrated Security=True;";
     
+    //Conexion RafaLAP [NO BORRAR]
+    private string connectionString ="Server=DESKTOP-3EG8250;Database=bancoDeSangre;Integrated Security=True;";
+    
     //Conexion Bolio [NO BORRAR]
     //private string connectionString ="Server=LAPTOP-TQH24RE4;Database=bancoDeSangre;Integrated Security=True;Encrpyt=False;";
 
     //Conxion Arturo [no borrar]
-    private string connectionString ="Server=LAPTOP-R2D2;Database=bancoDeSangre;Integrated Security=True;";
+    //private string connectionString ="Server=LAPTOP-R2D2;Database=bancoDeSangre;Integrated Security=True;";
 
     public SqlConnection conexion;
 
