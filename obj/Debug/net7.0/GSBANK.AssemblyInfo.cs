b98d2b6bfe5a3dfac40fedb4f9c5671859ccc41f
Alexis Alexander Bolio Chan <alexis.luna.8@outlook.com>
2024-03-14 18:37:27 -0700

    ConsultaEmparejamiento

--- obj/Debug/net7.0/GSBANK.AssemblyInfo.cs
+++ obj/Debug/net7.0/GSBANK.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GSBANK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457f97a66005e260761cf1e857dc012ace59eb3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GSBANK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GSBANK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
